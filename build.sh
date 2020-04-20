0a5fcbe5df8870f243c5587bfb6e8fdbc9775b36
richwats <richwats@cisco.com>
2020-04-20 20:55:38 +1000

    build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+sudo git pull
+sudo docker build containers/backend --no-cache -t dcnm_aclm:0.1
+cd imagedir
+sudo rm dcnm_aclm_frontend.zip
+sudo rm image.tar
+cd ../frontend
+sudo zip dcnm_aclm_frontend.zip ./* -r
+sudo mv dcnm_aclm_frontend.zip ../imagedir
+cd ../imagedir
+sudo docker image save dcnm_aclm:0.1 -o image.tar
+cd ..
+sudo tar -czvf dcnm_aclm.tar.gz imagedir/o
+sudo mv dcnm_aclm.tar.gz /home/cisco
